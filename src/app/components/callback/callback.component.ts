d418bab34d6205d29dd900b1ac5c094116a6b115
Su nombre <you@example.com>
2020-06-03 02:14:52 -0500

    app auth0 con google v0.1

---
+++ src/app/components/callback/callback.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+
+@Component({
+  selector: 'app-callback',
+  templateUrl: './callback.component.html',
+  styleUrls: ['./callback.component.scss'],
+})
+export class CallbackComponent implements OnInit {
+  constructor(public auth: AuthService) {}
+
+  ngOnInit(): void {
+    this.auth.handleAuthCallback();
+  }
+}
---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styles: [],
+})
+export class HeaderComponent implements OnInit {
+  constructor(public auth: AuthService) {}
+
+  ngOnInit(): void {}
+}
